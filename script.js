454ee717db51e58a3c96ce9363a9c309e8e84f51
Deepak Kushwaha <128895460+Codebuster0001@users.noreply.github.com>
2024-02-03 15:57:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer','Graphic Designer' ],
+    typeSpeed: 50,
+    backSpeed: 50,  
+    backDelay: 100,  
+    startDelay: 100,  
+    loop: true
+  }); 
+
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+
+// timeline
+
